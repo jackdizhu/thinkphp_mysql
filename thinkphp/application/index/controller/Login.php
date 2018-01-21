37e353ff20facecb2eb4b495b26f14acaebc245f
jackdizhu <376365334@qq.com>
2018-01-21 11:14:43 +0800

    init

---
+++ thinkphp/application/index/controller/Login.php
@@ -0,0 +1,30 @@
+<?php
+namespace app\index\controller;
+class Login {
+    public function index(){
+        $User = M('User');
+        // $this->show('index');
+        $arr['userName']=I('post.userName');
+        $w_arr['userName']=$arr['userName'];
+        $arr['password']=I('post.password');
+        $w_arr['password']=$arr['password'];
+
+        if($arr['userName'] && $arr['password']){
+
+            $id = M('User')->where($w_arr)->find();
+            if(!$id){
+                $arr['code'] = '2';
+                $arr['err'] = '用户名或密码错误';
+            }else{
+                $arr['code'] = '1';
+                session('userName',$arr['userName']);
+            }
+
+            $this->ajaxReturn($arr,'json');
+        }else{
+            $arr['err'] = '填写信息不全 . . ';
+            $arr['code'] = '3';
+            $this->ajaxReturn($arr,'json');
+        }
+    }
+}
