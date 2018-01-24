c6662aaddf82028ca882a5e2e8f38e1c7fa0c272
jackdizhu <376365334@qq.com>
2018-01-24 18:34:24 +0800

    jwt

--- thinkphp/application/index/controller/Login.php
+++ thinkphp/application/index/controller/Login.php
@@ -4,9 +4,30 @@ namespace app\index\controller;
 use think\Controller;
 use think\Db;
 use think\Request;
+use \extend\JWT\JWT;
+
 
 class Login extends Controller {
     public function index(){
+        $key = "example_key";
+        $token = array(
+            "iss" => "http://example.org",
+            "aud" => "http://example.com",
+            "iat" => 1356999524,
+            "nbf" => 1357000000
+        );
+
+        /**
+         * IMPORTANT:
+         * You must specify supported algorithms for your application. See
+         * https://tools.ietf.org/html/draft-ietf-jose-json-web-algorithms-40
+         * for a list of spec-compliant algorithms.
+         */
+        $jwt = JWT::encode($token, $key);
+        $decoded = JWT::decode($jwt, $key, array('HS256'));
+
+        print_r($decoded);die(0);
+
         $User = Db::name('User');
         // $this->show('index');
         $arr['userName']=input('post.userName');
