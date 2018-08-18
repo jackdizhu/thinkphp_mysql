537f51c6c627e10eb53b82997009101bce91fdcd
jackdizhu <376365334@qq.com>
2018-08-18 11:38:32 +0800

    增加 分页代码

--- thinkphp/Application/Api/Controller/IndexController.class.php
+++ thinkphp/Application/Api/Controller/IndexController.class.php
@@ -1,6 +1,7 @@
 <?php
 namespace API\Controller;
 use Think\Controller;
+use Think\Page;
 class IndexController extends Controller {
     public function index(){
         $User = M('User');
@@ -23,5 +24,33 @@ class IndexController extends Controller {
             $arr['code'] = '3';
             $this->ajaxReturn($arr,'json');
         }
+    }
+    public function page1(){
+        $User = M('User'); // 实例化User对象
+        $pageSize = 1;
+        $page = $_GET['page'] || 1;
+        // 进行分页数据查询 注意page方法的参数的前面部分是当前的页数使用 $_GET[p]获取
+        $list = $User->page($page, $pageSize)->select();
+        $count      = $User->count();// 查询满足要求的总记录数
+        $Page       = new Page($count, $pageSize);// 实例化分页类 传入总记录数和每页显示的记录数
+        $pageHtml       = $Page->show();// 分页显示输出
+
+        $data['list'] = $list;
+        $data['pageHtml'] = $pageHtml;
+        $this->ajaxReturn($data,'json');
+    }
+    public function page2(){
+        $User = M('User'); // 实例化User对象
+        $pageSize = 1;
+        $page = $_GET['page'] || 1;
+        $count      = $User->count();// 查询满足要求的总记录数
+        $Page       = new Page($count, $pageSize);// 实例化分页类 传入总记录数和每页显示的记录数5
+        $show       = $Page->show();// 分页显示输出
+        // 进行分页数据查询 注意limit方法的参数要使用Page类的属性
+        $list = $User->limit($Page->firstRow.','.$Page->listRows)->select();
+
+        $data['list'] = $list;
+        $data['show'] = $show;
+        $this->ajaxReturn($data,'json');
     }
 }
